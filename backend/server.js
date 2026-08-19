e937fb2a3095b2f25c30d96f063bc04ba9d9777d
Anjali Thakur <Sarojini46k@gmail.com>
2026-08-19 15:29:38 +0530

    Configure backend port for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const { getHotPosts } = require("./redditService");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.json({
